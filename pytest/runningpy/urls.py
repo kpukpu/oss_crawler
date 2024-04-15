45dfe3b916778f5d4f31940e6e402d587424e584
kpukpu <57974389+kpukpu@users.noreply.github.com>
2024-04-15 20:58:06 +0900

    Add files via upload

---
+++ pytest/runningpy/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from runningpy import views
+
+urlpatterns = [
+    path('',views.index),
+    path('calculate/',views.calculate),
+    path('result/',views.result),
+]
