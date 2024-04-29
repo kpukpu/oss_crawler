62e29448efd69b2a371669359fce707be50ca7ad
kpukpu <57974389+kpukpu@users.noreply.github.com>
2024-04-29 17:11:39 +0900

    Delete pytest directory
    기사 요약 기능 추가 업데이트

--- pytest/runningpy/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from runningpy import views
-
-urlpatterns = [
-    path('',views.index),
-    path('calculate/',views.calculate),
-    path('result/',views.result),
-]
